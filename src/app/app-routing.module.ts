85675ce512c5d606cc17a721ac981feabbb1b9c9
Orkhan.Ismayilov <orkhan.ismayilov@payever.org>
2022-04-04 00:05:34 +0400

    Added app routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+
+const routes: Routes = [
+  { path: '', component: PostsListComponent },
+  { path: 'create', component: PostCreateComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
