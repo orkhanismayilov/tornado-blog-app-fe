0ca83e10759f7389a6e1288d59c184178f0a6e5e
orkhanismayilov <orkhan.ismayilov@payever.org>
2022-04-16 23:35:53 +0400

    Added paginator Switched post content to lazy load mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,17 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ImagePickerComponent } from './image-picker/image-picker.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostsListComponent } from './posts/posts-list/posts-list.component';
-import { ImagePickerComponent } from './image-picker/image-picker.component';
 
 @NgModule({
   declarations: [
@@ -39,6 +39,7 @@ import { ImagePickerComponent } from './image-picker/image-picker.component';
     MatButtonModule,
     MatIconModule,
     MatProgressSpinnerModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
