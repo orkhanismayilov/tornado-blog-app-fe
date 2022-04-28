dc9181583211b7bd2f4e08a7427d99655eff2659
orkhanismayilov <orikismayilov@outlook.com>
2022-04-28 22:03:42 +0400

    Refactored loader service Redactored post & paginator interfaces

--- src/app/posts/interfaces/post.ts
+++ src/app/posts/interfaces/post.ts
@@ -2,6 +2,7 @@ export interface Post {
   id?: string;
   title: string;
   content: string;
+  image?: File;
   imagePath: string;
   createdAt: Date;
   updatedAt: Date;
--- src/app/services/loader.service.ts
+++ src/app/services/loader.service.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { BehaviorSubject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class LoaderService {
   isLoading$: BehaviorSubject<boolean> = new BehaviorSubject(false);
 }
