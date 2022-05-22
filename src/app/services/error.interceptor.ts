901534ed41fc14a4b6c34eb973a59ffb1038245f
orkhanismayilov <orikismayilov@outlook.com>
2022-05-22 21:55:51 +0400

    Removed unnecessary arguments in error interceptor

--- src/app/services/error.interceptor.ts
+++ src/app/services/error.interceptor.ts
@@ -7,6 +7,7 @@ import {
 import { Injectable } from '@angular/core';
 
 import { catchError, Observable, of } from 'rxjs';
+
 import { LoaderService } from './loader.service';
 import { SnackbarService } from './snackbar.service';
 
@@ -22,7 +23,7 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError((err) => {
         this.loaderService.isLoading$.next(false);
-        this.snackbarService.open(err.error.message ?? 'Unknown error', true, { duration: null });
+        this.snackbarService.open(err.error.message ?? 'Unknown error');
         return of(null);
       }),
     );
