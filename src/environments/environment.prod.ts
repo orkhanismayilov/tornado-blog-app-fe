da91590caed7ebc5d8c26168ee43ccf5f508e582
orkhanismayilov <orikismayilov@outlook.com>
2022-06-05 16:02:43 +0400

    Updated prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+const apiUrl = 'http://backend.3031303.xyz/api';
+
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    posts: `${apiUrl}/posts`,
+    auth: `${apiUrl}/auth`,
+  },
 };
