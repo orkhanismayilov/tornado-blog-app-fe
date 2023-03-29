b8e3eb3c9eb2f7038a5686f99741f64e92ed22b6
orkhanismayilov <orikismayilov@outlook.com>
2023-03-29 20:18:41 +0400

    Update angular to the latest version

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
 import {
   MAT_FORM_FIELD_DEFAULT_OPTIONS,
@@ -11,7 +12,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatDialogModule } from '@angular/material/dialog';
 import {
   MAT_SNACK_BAR_DEFAULT_OPTIONS,
   MatSnackBarModule,
